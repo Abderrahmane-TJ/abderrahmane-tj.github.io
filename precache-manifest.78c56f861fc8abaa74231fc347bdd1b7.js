5b0d83f311dd4733b421c020574f3d808183c824
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2019-09-19 17:00:06 +0200

    Updates

---
+++ precache-manifest.78c56f861fc8abaa74231fc347bdd1b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a81df2372c4f0c92f8f6657b722fac1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02cf9f7cb8a9a8673250",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2caca4d39d599a581904",
+    "url": "/static/css/main.6610cc88.chunk.css"
+  },
+  {
+    "revision": "02cf9f7cb8a9a8673250",
+    "url": "/static/js/2.36db1bc4.chunk.js"
+  },
+  {
+    "revision": "2caca4d39d599a581904",
+    "url": "/static/js/main.db2bad05.chunk.js"
+  },
+  {
+    "revision": "e340ab44acfdd0c6de99",
+    "url": "/static/js/runtime~main.e10dd4fd.js"
+  }
+]);
