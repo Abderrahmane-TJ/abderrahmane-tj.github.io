bef81580aaef0a884be053e0d44a9dbf22ff0012
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2019-09-19 22:26:36 +0200

    Updates

--- precache-manifest.c95670cd3f9d1f8c3a01989da584028b.js
+++ precache-manifest.c95670cd3f9d1f8c3a01989da584028b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c7a07a83ae33721628f58256eda6e014",
+    "revision": "194c88522b597d0840db9fbd91344a63",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "4f54f2c609b5c4a11dec",
-    "url": "/static/css/main.05d1d26f.chunk.css"
+    "revision": "40d86d44ae91fabc7048",
+    "url": "/static/css/main.4d2bb0e8.chunk.css"
   },
   {
     "revision": "acc7814159e7938b0203",
     "url": "/static/js/2.b673e87c.chunk.js"
   },
   {
-    "revision": "4f54f2c609b5c4a11dec",
-    "url": "/static/js/main.5f1cc610.chunk.js"
+    "revision": "40d86d44ae91fabc7048",
+    "url": "/static/js/main.78f314ef.chunk.js"
   },
   {
     "revision": "e340ab44acfdd0c6de99",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9602e0c0c934e1ad810966fe4065d827.js"
+  "/precache-manifest.c95670cd3f9d1f8c3a01989da584028b.js"
 );
 
 self.addEventListener('message', (event) => {
